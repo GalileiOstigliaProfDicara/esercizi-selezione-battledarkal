40cb36021d7781f474e968f8473128ee0d8cb669
battledarkal <steamjacopo31@gmail.com>
2025-11-25 13:37:22 +0000

    ifelse primo

--- es1.c
+++ es1.c
@@ -4,5 +4,16 @@
 
 int main(){
 
+    int = num1, num2;
+    printf("inserire due numeri:  ");
+    scanf("%c", &num1\n);
+    scanf("%c", &num2\n);
+    
+    if(num1 > num2){
+        printf("il primo numero è maggiore del secondo");
+    } else {
+        print("il primo numero è minore del secondo");
+    }
+    
     return 0;
 }
