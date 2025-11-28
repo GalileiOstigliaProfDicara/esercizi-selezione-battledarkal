cc2d56459ad8359630b7b9512f825cf2b0848206
battledarkal <steamjacopo31@gmail.com>
2025-11-28 11:49:45 +0100

    es2 finito

--- es2.c
+++ es2.c
@@ -3,6 +3,17 @@
 #include <stdio.h>
 
 int main(){
+    
+    int eta;
+
+    printf("inserire la propria età");
+    scanf("&d, &eta");
+    
+    if(eta>18){
+    printf("L'utente è maggiorenne");
+    } else {
+    printf("L'utente non è maggiorenne");
+    }
 
     return 0;
 }
